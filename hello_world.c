3efef693ac9817202c6e70fae94e357e35bce86e
Jack Robbers <jack@2pisoftware.com>
2022-06-03 12:51:08 +1000

    add hello_world.c example

---
+++ hello_world.c
@@ -0,0 +1,12 @@
+// Jack Robbers, z5263682, 2022.6.3
+
+#include <stdio.h>
+
+int main(void) {
+    printf(" _ _\n");
+    printf(" o o\n");
+    printf("  - \n");
+    printf("\\\\_//\n");
+    
+    return 0;
+}
