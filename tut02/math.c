30cab53e9b1d7424a4fc2a9d12aaba6a6c0c9bc3
catherinecheng02 <z5310517@ad.unsw.edu.au>
2022-06-10 12:10:29 +1000

    Create math.c

---
+++ tut02/math.c
@@ -0,0 +1,17 @@
+// Demo of arithmetic in C
+// Written by Catherine Cheng 
+// 10-06-2022
+
+#include <stdio.h> 
+
+int main (void) {
+    
+    printf("7/2 = %d\n", 7/2);
+    printf("(3.0/2) + 1 = %lf\n", 3.0/2 + 1);
+    printf("'a' + 5 = %d\n", 'a' + 5);
+    printf("'F' - 'A' + 'a' = %d\n", 'F' - 'A' + 'a');
+    printf("'F' - 'B' + 'b' = %d\n", 'F' - 'B' + 'b');
+    
+    // how to convert 'f' into 'F'
+    // 'f' - 'a' + 'A' = 'F'
+}
